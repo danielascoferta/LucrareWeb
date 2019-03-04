72d8453a645fa388c80d63b4d4b3c6a81ef93923
danielascoferta <32795707+danielascoferta@users.noreply.github.com>
2019-03-04 09:44:27 +0200

    Al doilea comentariu

--- WEB_LUCRARE/js/main.js
+++ WEB_LUCRARE/js/main.js
@@ -1,8 +1,4 @@
 class Worker{
-	// String = name;
-	// String = surname;
-	// number=hourRate;
-	// number=hours;
 
 	constructor(name,surname, hourRate, hours){
 		this.name = name;
@@ -12,22 +8,51 @@ class Worker{
 	}
 
 	getSalary(){
-		this.hourRate * this.hours;
+		return this.hourRate * this.hours;
 	} 
+
+	getName(){
+	return this.name;
+}
+
+getSurname(){
+	return this.surname;
+}
+
+getRate(){
+	return this.hourRate;
+}
+
+getHours(){
+	return this.hours;
+}
+increaseSalary(a){
+	this.hourRate += a;
+}
 
 }
 
 let worker = new Worker('Ion', 'Creanga', 10, 176);
-console.log(worker.name);
-console.log(worker.surname);//afișează 'Creangă'
-console.log(worker.hourRate);//afișează 10
-console.log(worker.hours);//afișează 31
+console.log('Primul lucrator: ');
+console.log(worker.getName());
+console.log(worker.getSurname());//afișează 'Creangă'
+console.log(worker.getRate());//afișează 10
+console.log(worker.getHours());//afișează 31();
+worker.increaseSalary(2);
 console.log(worker.getSalary());//afișează1760= 10*176
 
 //Punctul 2
 
-// let worker = new Worker1('Mihai', 'Eminescu', 8, 200);
-// console.log(worker.name);
-// console.log(worker.surname);
-// console.log(worker.hourRate);
-// console.log(worker.hours);
+let worker1 = new Worker('Mihai', 'Eminescu', 8, 200);
+console.log('Cel de-al doilea lucrator:');
+console.log(worker1.getName());
+console.log(worker1.getSurname());
+console.log(worker1.getRate());
+console.log(worker1.getHours());
+worker1.increaseSalary(2);
+console.log(worker1.getSalary());
+
+let SalaryMax = worker.getSalary()>worker1.getSalary()?worker.name:worker1.name;
+console.log('Cel mai mare salariu il are: ', SalaryMax);
+
+
